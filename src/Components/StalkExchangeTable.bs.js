2b8d11e05a67bf77b986ee01fbd4f14d204c8e4f
Drew Carver <drew.carver@outlook.com>
2020-05-16 22:31:07 -0400

    ignore bucklescript files

--- src/Components/StalkExchangeTable.bs.js
+++ src/Components/StalkExchangeTable.bs.js
@@ -5,7 +5,7 @@ var React = require("react");
 var Belt_List = require("bs-platform/lib/js/belt_List.js");
 var RootReducer$ReasonReactExamples = require("../Redux/RootReducer.bs.js");
 var StalkExchangeRow$ReasonReactExamples = require("./StalkExchangeRow.bs.js");
-var UpdateStalkMarketPriceModal$ReasonReactExamples = require("./UpdateStalkMarketPriceModal.bs.js");
+var AddNewBrokerModal$ReasonReactExamples = require("./AddNewBrokerModal.bs.js");
 
 function marketsSelector(state) {
   return state.markets;
@@ -13,23 +13,23 @@ function marketsSelector(state) {
 
 function StalkExchangeTable(Props) {
   var markets = Curry._1(RootReducer$ReasonReactExamples.StalkExchangeStore.useSelector, marketsSelector);
-  var match = React.useState((function () {
-          return false;
-        }));
-  var setIsOpen = match[1];
   var marketRows = Belt_List.toArray(Belt_List.map(markets, (function (marketRow) {
               return React.createElement(StalkExchangeRow$ReasonReactExamples.make, {
                           marketRow: marketRow,
                           key: marketRow.stalkBroker
                         });
             })));
+  var match = React.useState((function () {
+          return false;
+        }));
+  var setIsOpen = match[1];
   return React.createElement("div", undefined, React.createElement("button", {
                   onClick: (function (param) {
                       return Curry._1(setIsOpen, (function (param) {
                                     return true;
                                   }));
                     })
-                }, "Add New Broker"), React.createElement(UpdateStalkMarketPriceModal$ReasonReactExamples.make, {
+                }, "Add New Broker"), React.createElement(AddNewBrokerModal$ReasonReactExamples.make, {
                   isOpen: match[0],
                   onClose: (function (param) {
                       return Curry._1(setIsOpen, (function (param) {
--- src/Components/StalkExchangeTable.re
+++ src/Components/StalkExchangeTable.re
@@ -2,14 +2,14 @@ let marketsSelector = (state: AppState.stalkExchangeState) => state.markets;
 
 [@react.component]
 let make = () => {
-  open RootReducer;
-  let markets = StalkExchangeStore.useSelector(marketsSelector);
+  let markets = RootReducer.StalkExchangeStore.useSelector(marketsSelector);
+  let marketRows = Belt.List.map(markets, marketRow => <StalkExchangeRow key=marketRow.stalkBroker marketRow/>) 
+    |> Belt.List.toArray;
   let (isOpen, setIsOpen) = React.useState(() => false);
-  let marketRows = Belt.List.map(markets, marketRow => <StalkExchangeRow key=marketRow.stalkBroker marketRow/>) |> Belt.List.toArray;
 
   <div>
     <button onClick={_ => setIsOpen(_ => true)}>"Add New Broker" -> React.string</button>
-    <UpdateStalkMarketPriceModal isOpen onClose={_ => setIsOpen(_ => false)} />
+    <AddNewBrokerModal isOpen onClose={_ => setIsOpen(_ => false)} />
     <table className="stalk-market-table">
       <thead>
         <tr className="stalk-market-table__header-row">
